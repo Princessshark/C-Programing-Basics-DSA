49a19e00528076e1048bd31226e50fd606f0ce88
Bipul Sharma <106522978+Princessshark@users.noreply.github.com>
2024-07-04 05:04:11 +0530

    101

---
+++ c.Temp101/source.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// Starting from the basics
+// Output's the text and number inside printf function into the Terminal/Console/commandLine
+int main() {
+	printf("C programing Basics & DSA %d\n", 101);
+	return 0;
+}
+
