2defef737a94d29c8da6d091de0a21e94c6c5ec0
Bipul Sharma <106522978+Princessshark@users.noreply.github.com>
2024-08-14 03:10:15 +0530

    exponentation
    using #include<math.h> and pow() to buffing up math ability and and an explicit type conversion, also known as type casting, float to int.

---
+++ c.Temp102/know_your_exponent.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+	int a,b;
+double c;
+
+	a = 2;
+	b = 2;
+
+	// exponentiation in C using pow()
+	c = pow(a, b);
+
+	printf("%lf \n", c);
+
+	printf("%d \n", (int)c);
+    
+
+	return 0;
+}
