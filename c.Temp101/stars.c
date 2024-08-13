657104bcabb205923786808261460b6d494eaf7f
Bipul Sharma <106522978+Princessshark@users.noreply.github.com>
2024-08-14 02:52:09 +0530

    Create stars.c
    Prints out a bunch of stars

---
+++ c.Temp101/stars.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+// Printing a pattern using '*'
+
+int main() {
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("****\n");
+    printf("*****\n");
+    printf("******\n");
+    return 0;
+}
