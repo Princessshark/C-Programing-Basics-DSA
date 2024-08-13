a0a5b420d6fadc7a353f5f2ec95abff09de21838
Bipul Sharma <106522978+Princessshark@users.noreply.github.com>
2024-08-14 03:02:17 +0530

    Create uinput_summation_only.c
    Taking two user defined integers and summing it together and outputting the sum.

---
+++ c.Temp102/uinput_summation_only.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+
+	int a, z;
+
+	printf("operand 1: ");    
+	scanf("%d", &a);
+
+	printf("operand 2: ");
+	scanf("%d", &z);
+
+	int sum = a + z;
+
+	printf("%d \n", sum);
+	return 0;
+}
