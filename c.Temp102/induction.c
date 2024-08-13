7395282236d6e8564b3dc4eec2d97be62c4eebfb
Bipul Sharma <106522978+Princessshark@users.noreply.github.com>
2024-08-14 03:04:11 +0530

    induction.c
    Inductive hypothesis brain teaser (easy)

---
+++ c.Temp102/induction.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+
+	int x, y, z;
+
+	x = 5;
+	y = 5;
+	z = 2;
+	
+printf("By learning of the operators figure out the operands\n");
+	printf("x * Z = %d \n", x * z);    
+	printf("x + y = %d \n", x + y);
+	printf("y + x = %d \n", y + x);
+	printf("y * z = %d \n", y * z);
+
+	return 0;
+}
