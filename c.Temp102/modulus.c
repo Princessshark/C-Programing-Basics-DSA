0ee8e00c76f44e896172a4614dad83290b3c7ae4
Bipul Sharma <106522978+Princessshark@users.noreply.github.com>
2024-08-22 08:24:21 +0530

    modulus.c
    % is the modulus operator, to out put the remainders

---
+++ c.Temp102/modulus.c
@@ -0,0 +1,55 @@
+int main() {
+	printf("%d \n", 1 % 1);
+	printf("%d \n", 2 % 1);
+	printf("%d \n", 3 % 1);
+	printf("%d \n", 4 % 1);
+	printf("%d \n", 5 % 1);
+	printf("%d \n", 6 % 1);
+	printf("%d \n", 7 % 1);
+	printf("%d \n", 8 % 1);
+	printf("%d \n", 9 % 1);
+	printf("%d \n", 0 % 1);
+	printf("Switch \n");
+	printf("%d \n", 500 % 2);
+	printf("%d \n", 501 % 2);
+	printf("%d \n", 502 % 2);
+	printf("%d \n", 503 % 2);
+	printf("%d \n", 504 % 2);
+	printf("%d \n", 505 % 2);
+	printf("%d \n", 506 % 2);
+	printf("%d \n", 507 % 2);
+	printf("%d \n", 508 % 2);
+	printf("%d \n", 509 % 2);
+	printf("%d \n", 510 % 2);
+	printf("Switch \n");
+
+
+	printf("%d \n", 500 % 3);
+	printf("%d \n", 501 % 3);
+	printf("%d \n", 502 % 3);
+	printf("%d \n", 503 % 3);
+	printf("%d \n", 504 % 3);
+	printf("%d \n", 505 % 3);
+	printf("%d \n", 506 % 3);
+	printf("%d \n", 507 % 3);
+	printf("%d \n", 508 % 3);
+	printf("%d \n", 509 % 3);
+	printf("%d \n", 510 % 3);
+
+
+	printf("switch \n");
+	printf("%d \n", 50 % 1);
+	printf("%d \n", 51 % 1);
+	printf("%d \n", 52 % 1);
+	printf("%d \n", 53 % 1);
+	printf("%d \n", 54 % 1);
+	printf("%d \n", 55 % 1);
+	printf("%d \n", 56 % 1);
+	printf("%d \n", 57 % 1);
+	printf("%d \n", 58 % 1);
+	printf("%d \n", 59 % 1);
+	printf("%d \n", 60 % 1);
+	printf("Switch \n");
+
+	return 0;
+}
