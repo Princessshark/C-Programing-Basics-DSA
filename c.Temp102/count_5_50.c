a6c94fd9699d692d21dff0f09148686a690ef430
Bipul Sharma <106522978+Princessshark@users.noreply.github.com>
2024-10-02 03:21:51 +0530

    Level 2
    count odd number from 5 to 50

---
+++ c.Temp102/count_5_50.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+
+int main() {
+
+
+	for (int index = 5; index <= 50; index = index + 1) {
+		if (index % 2 == 0) {
+			printf("%d\n", index);
+		} else {
+			printf("Even Number");
+		}
+		
+	}  
+
+
+
+
+	return 0;
+}
+
+
+
